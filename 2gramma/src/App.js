d1259161a296d9bfaf8c25d1515d1ce974f8976d
breaz1 <breaz1@yandex.ru>
2023-11-22 11:47:01 +0500

    Message Box

--- 2gramma/src/App.js
+++ 2gramma/src/App.js
@@ -11,10 +11,10 @@ function App() {
         <Container className="bebeb justify-between gap-10 h-full">
           <SelectDialog />
           <div className="rounded-3xl d-flex flex-column w-full justify-between gap-4">
-              <div className=" h-[70vh] lg:h-96">
+              <div className=" h-[70vh]">
                   <MessageBox />
               </div>
-              <div className="d-flex  gap-3">
+              <div className="d-flex gap-3">
                 <input className="w-full bg-white rounded-3xl" />
                 <button className=" bg-white rounded-full p-3"><img className="text-white" src="./sendIcon.svg" alt="отправить сообщение"/></button>
               </div>      
--- 2gramma/src/components/selectDialog.js
+++ 2gramma/src/components/selectDialog.js
@@ -2,7 +2,7 @@ import Nav from 'react-bootstrap/Nav';
 
 function SelectDialog() {
   return (
-    <Nav variant="underline" defaultActiveKey="/home" className='flex-column w-60 '>
+    <Nav variant="underline" defaultActiveKey="/home" className='bebe w-60 '>
       <Nav.Item className=''>
         <Nav.Link href="/home"  className=' text-white'>Общий чат</Nav.Link>
       </Nav.Item>
